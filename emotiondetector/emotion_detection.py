01dfd9f72ce2a9a0aaed66185cfa0afb939a6d01
Thom Carlos <john.thomas.carlos0913@gmail.com>
2024-07-29 21:21:11 +0800

    Added usage warning message

--- emotiondetector/emotion_detection.py
+++ emotiondetector/emotion_detection.py
@@ -5,7 +5,7 @@ import json
 import requests
 
 # NOTE: Don't try to use this
-# This won't work unless you are in the labs environment
+# This won't work unless you are in the lab's environment
 URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
